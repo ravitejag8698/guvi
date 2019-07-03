5d0a18b63bb2a9bb3c0f7f09be86da3e7bab8f3b
ravitejag8698 <52479255+ravitejag8698@users.noreply.github.com>
2019-07-03 10:21:21 +0530

    Create guvi4.py

---
+++ guvi4.py
@@ -0,0 +1,6 @@
+alpha = input('')
+li = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+if alpha in li:
+  print('Alphabet')
+else:
+  print('No')
