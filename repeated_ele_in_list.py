941a7779ed121da35246d9caa9616bbb4a630b76
ravitejag8698 <52479255+ravitejag8698@users.noreply.github.com>
2019-07-04 20:00:06 +0530

    Update repeated_ele_in_list.py

--- repeated_ele_in_list.py
+++ repeated_ele_in_list.py
@@ -1,5 +1,15 @@
 n = int(input())
 s = map(int,input().split())
 li = list(s)
+repeat = []
 for i in range(n):
-  for j in range(i,n)
+  for j in range(i+1,n):
+    if li[i] == li[j]:
+      repeat.append(li[i])
+r = len(repeat)
+if(r==0):
+  print('unique')
+else:
+  for x in repeat:
+    print(x,end=' ')
+      
