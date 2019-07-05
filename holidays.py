51526c865f56007065f452c34bb04b34c939490d
ravitejag8698 <52479255+ravitejag8698@users.noreply.github.com>
2019-07-05 21:53:15 +0530

    Update holidays.py

--- holidays.py
+++ holidays.py
@@ -1,10 +1,5 @@
-d,f = map(str,input().split())
+d = input()
 if (d == 'Sunday' or d == 'Saturday'):
   print('yes')
 else:
   print('no')
-if (f == 'Sunday' or f == 'Saturday'):
-  print('yes')
-else:
-  print('no')
- 
