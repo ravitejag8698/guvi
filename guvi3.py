28bf5352b6951eae58ee071bbd2d2fbdbafec1f1
ravitejag8698 <52479255+ravitejag8698@users.noreply.github.com>
2019-07-05 20:16:09 +0530

    Update guvi3.py

--- guvi3.py
+++ guvi3.py
@@ -1,9 +1,9 @@
-vowel = ['a','e','i','o','u']
-consonant = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','u','v','w','x','y','z']
+vowels = ['a','e','i','o','u']
+consonants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','u','v','w','x','y','z']
 char = input('')
-if char in vowel:
+if char in vowels:
   print('Vowel')
-elif char in consonant:
+elif char in consonants:
   print('Consonant')
 else:
   print('invalid')
