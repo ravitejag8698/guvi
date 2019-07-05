5219eef043faa96f31ba2b060adcfb6cf1d16bba
ravitejag8698 <52479255+ravitejag8698@users.noreply.github.com>
2019-07-05 11:08:39 +0530

    Create uniquenum.py

---
+++ uniquenum.py
@@ -0,0 +1,14 @@
+
+n = int(input())
+l = map(int,input().split())
+ls = list(l)
+dict = {}
+for i in ls:
+  if i in dict:
+    dict[i] += 1
+  else:
+    dict[i] = 1
+for key,val in dict.tems():
+  if val == 1:
+    print(key,end=' ')
+   
