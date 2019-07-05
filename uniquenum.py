3719b4a6ea9624c528641b0d0c84d68418c86e12
ravitejag8698 <52479255+ravitejag8698@users.noreply.github.com>
2019-07-05 18:21:19 +0530

    Update uniquenum.py

--- uniquenum.py
+++ uniquenum.py
@@ -3,12 +3,17 @@ n = int(input())
 l = map(int,input().split())
 ls = list(l)
 dict = {}
-for i in ls:  
-  if i in dict:
-    dict[i] += 1
-  else:
-    dict[i] = 1
+for i in ls:
+    
+    
+    if i in dict:
+        dict[i] += 1
+    else:
+        
+        dict[i] = 1
 for key,val in dict.tems():
-  if val == 1:
-    print(key,end=' ')
+    
+    if val == 1:
+        
+        print(key,end=' ')
    
