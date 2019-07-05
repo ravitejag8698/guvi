110b0202b882e2c398a099cc6b4db8621bc85e00
ravitejag8698 <52479255+ravitejag8698@users.noreply.github.com>
2019-07-05 18:39:33 +0530

    Create firstrepeat.py

---
+++ firstrepeat.py
@@ -0,0 +1,15 @@
+n = int(input())
+f = map(int,input().split())
+d = list(f)
+k = []
+for i in range(n):
+  for j in range(i+1,n):
+    if d[i] == d[j]:
+      k.append(d[i])
+      break
+  break
+  
+if(len(k) == 0):
+  print('unique')
+else:
+  print(k[0])
