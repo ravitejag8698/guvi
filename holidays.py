4f6ad1225a987e04f09d254ac8b4c9db6b1f8142
ravitejag8698 <52479255+ravitejag8698@users.noreply.github.com>
2019-07-05 21:40:59 +0530

    Create holidays.py

---
+++ holidays.py
@@ -0,0 +1,10 @@
+d,f = map(int,input().split())
+if (d == 'Sunday' or d == 'Saturday'):
+  print('yes')
+else:
+  print('no')
+if (f == 'Sunday' or f == 'Saturday'):
+  print('yes')
+else:
+  print('no')
+ 
