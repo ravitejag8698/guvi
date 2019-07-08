40808fe4c72a31e1bae187d6a0e752c33ed54365
ravitejag8698 <52479255+ravitejag8698@users.noreply.github.com>
2019-07-08 12:10:47 +0530

    Create adduptoaspecific_target_in_list.py

---
+++ adduptoaspecific_target_in_list.py
@@ -0,0 +1,13 @@
+n,k = map(int,input().split())
+d = map(int,input().split())
+d = list(d)
+repeat = []
+for i in range(n):
+  for j in range(i+1,n):
+    if d[i] + d[j] == k:
+      repeat.append([d[i],d[j])
+if (len(repeat) == 0):
+  print('NO')
+else:
+  print('YES')
+      
