35416c3cf032839507a02c8133d1132d28d2178e
ravitejag8698 <52479255+ravitejag8698@users.noreply.github.com>
2019-07-09 14:45:53 +0530

    Update permutation.py

--- permutation.py
+++ permutation.py
@@ -1,6 +1,13 @@
 from itertools import permutations
 n = input()
+per = []
 permlist = permutations(n)
 for perm in list(permlist):
-  print(''.join(perm)
   
+  f = ''.join(perm)
+  per.append(f)
+per = set(per)
+per = list(per)
+per.sort()
+for z in per:
+  print(z)
